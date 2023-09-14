e8bb5f79ee4373f45eea038b541aa30be803dfc3
Dutta, Hemanta <hemanta.kumar.dutta@accenture.com>
2023-09-14 13:50:25 +0530

    Practice

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+
+@Injectable()
+
+export class CourseService {
+    getCourses (): Observable<Course>{
+        return
+    }
+}
